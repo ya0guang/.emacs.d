823a364f3ff6c717e20cdcec6d042b4c030e24eb
ya0guang <ya0guang@protonmail.com>
2022-05-05 02:59:44 -0400

    add coq and rust suport

---
+++ README.md
@@ -0,0 +1,39 @@
+# My Configuration
+
+- line numebr for coding
+- line:column in mode line (doom)
+
+# Language Support
+
+**Global Plugins:**
+- Yasnippet
+- company
+- lsp-mode
+
+## Markdown
+
+## Rust
+
+- rust-mode
+
+## Coq
+
+- proof-general
+- company-coq
+- pretty-mode
+
+## C
+
+TODO
+
+## C++
+
+TODO
+
+## Python
+
+TODO
+
+## Fstar
+
+TODO
--- init.el
+++ init.el
@@ -9,7 +9,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (lsp-mode rust-mode doom-modeline))))
+ '(package-selected-packages
+   (quote
+    (company-coq pretty-mode proof-general yasnippet-snippets yasnippet company markdown-preview-mode lsp-mode rust-mode doom-modeline))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,18 +19,31 @@
  ;; If there is more than one, they won't work right.
  )
 
+; Global
 (require 'doom-modeline)
 (doom-modeline-mode 1)
+
+(require 'lsp-mode)
 
 (when (display-graphic-p)
   (require 'all-the-icons))
 
 (setq column-number-mode t)
-
 (setq line-number-mode t)
 
+(yas-reload-all)
+
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'prog-mode-hook #'yas-minor-mode)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
 ;; Rust mode settings
+(add-to-list 'exec-path "~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin")
 (require 'rust-mode)
 (setq rust-format-on-save t)
+(add-hook 'rust-mode-hook #'lsp)
+
+;; Coq
+(add-hook 'coq-mode-hook #'company-coq-mode)
+(add-hook 'coq-mode-hook #'prettify-symbols-mode)
+
