2619fb185d4815284f3c212502af1039fc6e5a7b
ya0guang <ya0guang@protonmail.com>
2022-06-15 16:21:20 -0400

    use org-mode and spellchecker

--- init.el
+++ init.el
@@ -28,17 +28,18 @@
 ;; Global
 (straight-use-package 'doom-modeline)
 (straight-use-package 'yasnippet)
+(straight-use-package 'all-the-icons)
 (require 'doom-modeline)
 (doom-modeline-mode 1)
 
 (straight-use-package 'lsp-mode)
 (require 'lsp-mode)
 
-(when (display-graphic-p)
-  (require 'all-the-icons))
+;; (when (display-graphic-p)
+;;   (require 'all-the-icons))
 
 (setq column-number-mode t)
-(setq line-number-mode t)
+(setq line-number-mode t) 
 
 (yas-reload-all)
 
@@ -46,6 +47,15 @@
 (add-hook 'after-init-hook 'global-company-mode)
 (add-hook 'prog-mode-hook #'yas-minor-mode)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+;; spellcheck
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+;; org mode
+(straight-use-package 'org-mode)
+(setq org-agenda-files (directory-files-recursively "~/Code/" "\\.org$"))
+(setq org-todo-keywords
+      '((sequence "NEXT" "TODO" "WAITING" "SOMEDAY" "VERIFY" "|" "DONE" "CANCELLED")))
 
 ;; Rust mode settings
 (add-to-list 'exec-path "~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin")
@@ -68,3 +78,16 @@
 
 ;; C++
 
+;; Editor
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:slant normal :weight normal :height 140 :width normal)))))
